9d4e1321ab08407668cf699f31c5d57847c524fc
kurtyazdizadeh <kurtyazdizadeh@gmail.com>
2020-05-29 18:06:45 +0000

    changed pointer class to only be on image ands and color options on the product cards on main page

--- client/components/product-list-item.jsx
+++ client/components/product-list-item.jsx
@@ -6,13 +6,15 @@ class ProductListItem extends React.Component {
     const { name, price, desc, images, colorOptions, productId } = this.props;
     return (
       <div
-        className="card product-item m-2 col-11 col-md-5 col-lg-3 pointer"
+        className="card product-item m-2 col-11 col-md-5 col-lg-3"
         // onClick={() => {
         //   setView(name, { productId: productId });
         // }}
       >
-        <img src={`./images/${productId}/${colorOptions[0]}-${images[1]}.webp`} className="card-img-top mt-3 scale img-fluid bottom" alt={name} />
-        <img src={`./images/${productId}/${colorOptions[0]}-${images[0]}.webp`} className="card-img-top mt-3 scale img-fluid top" alt={name} />
+        <div className="card-image pointer">
+          <img src={`./images/${productId}/${colorOptions[0]}-${images[1]}.webp`} className="card-img-top mt-3 scale img-fluid bottom" alt={name} />
+          <img src={`./images/${productId}/${colorOptions[0]}-${images[0]}.webp`} className="card-img-top mt-3 scale img-fluid top" alt={name} />
+        </div>
         <div className="card-body">
           <div className="d-flex">
             <div className="mr-auto">
@@ -23,7 +25,7 @@ class ProductListItem extends React.Component {
               <h6 className="my-1">Colors:</h6>
               {
                 colorOptions.map((color, index) => {
-                  return <div key={index} className={`${color} color ml-0`}></div>;
+                  return <div key={index} className={`${color} color ml-0 pointer`}></div>;
                 })
               }
             </div>
